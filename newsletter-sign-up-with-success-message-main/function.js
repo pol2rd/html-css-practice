e490ddd939bb01012a3a03cc568e0af8ef7494a6
LirPes <lironpes@@gmail.com>
2025-11-20 11:33:47 +0200

    add success window desgin and functionality

--- newsletter-sign-up-with-success-message-main/function.js
+++ newsletter-sign-up-with-success-message-main/function.js
@@ -1,12 +1,18 @@
 function send(event) {
     event.preventDefault();
-    const form = event.target;
-    const data = new FormData(form);
-    const email = data.get("email-address");
-    console.log(email)
-    const subscribeForm = document.querySelector(".subscribe-form");
-    const subscribeSuccessWindow = document.querySelector(".subscribe-success");
-    subscribeForm.classList.add("active");
-    document.querySelector(".subscribe-success p b").textContent = email;
-    subscribeSuccessWindow.classList.add("active");
+    const emailInput = document.getElementById("email-address");
+    console.log(emailInput.checkValidity())
+    if(!emailInput.checkValidity()) {
+        emailInput.classList.add("invalid");
+        document.getElementById(".inalid-email-label").classList.add("invalid");
+    } else {
+        const form = event.target;
+        const data = new FormData(form);
+        const email = data.get("email-address");
+        const subscribeForm = document.querySelector(".subscribe-form");
+        const subscribeSuccessWindow = document.querySelector(".subscribe-success");
+        subscribeForm.classList.add("active");
+        document.querySelector(".subscribe-success p b").textContent = email;
+        subscribeSuccessWindow.classList.add("active");
+    }
 }
